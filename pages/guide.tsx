8b59f60293b9d86939f77b15c5dacabc422464d7
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-12-20 20:10:24 +0900

    💄 모달창 생성 작업

--- pages/guide.tsx
+++ pages/guide.tsx
@@ -44,8 +44,15 @@ function guide() {
                 </div>
               </>
             ) : null}
-
-            <div className={styles.text} />
+            <div className={styles.text}>
+              <p>
+                안녕 나는 토비야. 너의 가치와 우선순위를 고려해보는 게 중요해.
+                미래에 후회하지 않기 위해서는 본인의 목표와 가치에 맞는 선택을
+                하면 돼. 다양한 가능성을 탐색하고 네 직감에 따라 결정해봐.
+                중요한 건 네가 마음으로 편안하고 확신 있는 결정을 내릴 수 있게
+                하는 거야. 힘내 친구야...
+              </p>
+            </div>
             {number === 2 ? (
               <>
                 <div className={styles.top} />
