0842fc26b56d0b9216e4c1285b67d740a8e31b3c
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2024-03-21 13:19:36 +0900

    fix: commonMetaData 불러오기 수정

--- src/app/success/layout.tsx
+++ src/app/success/layout.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from 'next'
-import { commonMetaData } from '@/meta-data/meta-data'
+import { Metadata } from "next";
+import commonMetaData from "@/meta-data/meta-data";
 
 export const metadata: Metadata = {
   ...commonMetaData,
-}
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return <> {children}</>;
 }
