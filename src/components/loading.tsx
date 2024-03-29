1450bc555e4ef9e1d2cb7224b33f9de1a337240a
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2024-03-29 18:06:17 +0900

    feat: Dots 컴포넌트 생성 및 적용

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import characterSelector from "@/recoil/selector/characterSelector";
 import styles from "../styles/loading.module.scss";
+import Dots from "./Dots";
 
 function loading() {
   const CHARACTERSTATUS = useRecoilValue(characterSelector);
+  const [imgNum, setImgNum] = useState(0);
+  useEffect(() => {
+    setImgNum(CHARACTERSTATUS);
+  }, [CHARACTERSTATUS]);
   return (
     <div className={styles.background}>
       <div className={styles.loading}>
-        <div className={styles.dots}>
-          <div className={styles.droplet} />
-          <div className={styles.droplet} />
-          <div className={styles.droplet} />
+        <div className={styles.dotBox}>
+          <Dots />
         </div>
         <img src="/images/loading.svg" alt="loading" />
       </div>
       <div className={styles.character}>
-        <img src={`/images/character${CHARACTERSTATUS}.svg`} alt="character" />
+        <img src={`/images/character${imgNum}.svg`} alt="character" />
       </div>
       <div className={styles.comment}>캐릭터 문구</div>
     </div>
