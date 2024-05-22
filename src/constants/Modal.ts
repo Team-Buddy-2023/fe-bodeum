546b916050bdbf96c571669095526ff83dc6c8e6
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2024-05-22 15:02:48 +0900

    feat: 채팅을 안했을때, 선택한 대화가 없을때 안내 팝업 메세지

--- src/constants/Modal.ts
+++ src/constants/Modal.ts
@@ -19,6 +19,16 @@ const MODAL = {
     BUTTON1: "돌아가기",
     BUTTON2: "공유하기",
   },
+  NOCHAT: <DATA>{
+    TEXT: "대화를 나누지 않았습니다. 종료하시겠습니까?",
+    BUTTON1: "돌아가기",
+    BUTTON2: "종료하기",
+  },
+  NOSELECTSHARE: <DATA>{
+    TEXT: "선택된 대화가 없습니다. 대화를 선택해주세요.",
+    BUTTON1: "돌아가기",
+    BUTTON2: "",
+  },
   HOME: <DATA>{
     TEXT: "지금 홈으로 이동하면 다시 결과창으로 돌아올 수 없어요",
     BUTTON1: "돌아가기",
